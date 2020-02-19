b7637ca46418e1e4be31bf17f6b195b7d3f50eed
Richard han <hxy.314@gmail.com>
2020-02-19 13:27:36 +1100

    Update typescript.js

--- typescript.js
+++ typescript.js
@@ -1,1 +1,32 @@
 
+```
+
+export type IconProps = {
+	content: string;
+	id?: string;
+	viewBox: string;
+	isMounted: boolean;
+};
+```
+```
+type AccordionStateProps = {
+	isOpen: boolean;
+};
+```
+```
+export class Accordion extends React.Component<Props, AccordionStateProps> {
+constructor(props: Props) {
+		super(props);
+	}
+render() {
+		const {
+			children,
+			customButtonComponent,
+			isOpen: isOpenProp,
+			onChange: onChangeProp,
+			...restProps
+		} = this.props;
+return ()}
+
+export default Accordion;
+```
